18dfa15bfa1036f6697d595b39541dc99b2b2029
Jose Eduardo <joseeduardo.jeg37@gmail.com>
2023-01-26 12:49:43 -0600

    animated

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,9 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInDownOnEnterAnimation, fadeInOnEnterAnimation, fadeInUpOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'public',
   templateUrl: './public.component.html',
-  styleUrls: ['./public.component.scss']
+  styleUrls: ['./public.component.scss'],
+  animations:[
+    fadeInDownOnEnterAnimation(
+      {
+        anchor: 'down',
+        duration: 800,
+        delay: 200,
+        translate: '10%'
+      }
+    ),
+    fadeInUpOnEnterAnimation(
+      {
+        anchor: 'up',
+        duration: 800,
+        delay: 200,
+        translate: '10%'
+      }
+    ),
+      fadeInOnEnterAnimation(
+        {
+          anchor: 'fade',
+          animateChildren: 'after',
+          duration: 200,
+        }
+      )
+  ]
 })
 export class PublicComponent implements OnInit {
 
